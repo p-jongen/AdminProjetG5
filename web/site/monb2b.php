d8890992b21ca4e55a041a26cb01b951568a493b
p-jongen <philemon.philj@gmail.com>
2018-05-06 15:47:16 +0000

    Ajout des configs et des pages

---
+++ web/site/monb2b.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Base</title>
+    <style>
+        html{
+            background-color: green;
+            font-family: Verdana, Tahoma, sans-serif;
+        }
+        h1 {
+            color: black;
+            text-align: center;
+        }
+        #menu{
+            list-style: square;
+        }
+        a:hover, a:focus {
+            color: orangered;
+    </style>
+</head>
+<body>
+<div id="principal">
+    <header>
+        <h1>Site B2B</h1>
+        <p id="menu">
+        <ul>
+            <li><a href="Base.php">Retour</a></li>
+            <li><a href="index.html">Clients</a></li>
+            <li><a href="liste.html">Shop</a></li>
+            <li><a href="utiliser.html">Pièces</a></li>
+        </ul>
+        </p>
+    </header>
+    <main>
+    </main>
+    <footer>
+    </footer>
+</div>
+</body>
+</html>
